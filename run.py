835f9e299376bcf9993c1ae9d7d416c09604e5ff
pieterkdevilliers <pieterkdevilliers@me.com>
2022-08-30 15:23:00 +0000

    Write surplus data to surplus spreadsheet

--- run.py
+++ run.py
@@ -32,8 +32,7 @@ def get_sales_data():
         data_str = input("Enter your data here: ")
         
         sales_data = data_str.split(",")
-        
-        
+
         if validate_data(sales_data):
             print("Data is valid!")
             break
@@ -83,6 +82,15 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return surplus_data
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided.
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
 
 def main():
     """
@@ -92,7 +100,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation.\n")
